8ad6e91c3a423540bc9f0bdef3c32981fdb35151
Tommo <tomcreer@gmail.com>
2020-12-03 18:09:03 +0000

    changes

--- setup.sh
+++ setup.sh
@@ -2,8 +2,10 @@ mkdir -p ~/.streamlit/
 echo "\
 [general]\n\
 email = \"tomcreer@gmail.com\"\n\
+" > ~/.streamlit/credentials.toml
+echo "\
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
 port = $PORT\n\
-" > ~/.streamlit/credentials.toml
+" > ~/.streamlit/config.toml
